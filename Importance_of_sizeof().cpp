35fa4df46bd7f633d5ed352c6db7278fcf143fe3
chdaksh01 <96689263+chdaksh01@users.noreply.github.com>
2024-05-13 07:16:38 +0530

    Create Importance_of_sizeof().cpp

---
+++ Importance_of_sizeof().cpp
@@ -0,0 +1,21 @@
+  // IMPORTANCE OF sizeof() function
+  
+  // Find Dimensions of a 2D array
+  
+  int main(int argc, char const *argv[])
+  {
+      int arr[6][5] = {
+          {1,2,3,4,5},
+          {1,2,3,4,5},
+          {1,2,3,4,5},
+          {1,2,3,4,5},
+          {1,2,3,4,5},
+          {1,2,3,4,5}
+      };
+    
+      int rows = sizeof(arr)/sizeof(arr[0]);
+      int cols = sizeof(arr[0])/sizeof(arr[0][0]);
+    
+      cout<<rows<<" "<<cols<<endl;
+      return 0;
+  }
