f4f5b9ef9bb28c6fe65e170be1d19259315b38b2
chdaksh01 <96689263+chdaksh01@users.noreply.github.com>
2024-05-16 09:02:53 +0530

    Create mp.count() mp.find().cpp

---
+++ Conceptual/mp.count() mp.find().cpp
@@ -0,0 +1,24 @@
+/*
+    The map::count() is a built-in function in C++ STL which returns 1 if the element with key K is present 
+    in the map container. It returns 0 if the element with key K is not present in the container.
+*/
+
+/*
+    The map::find() is a built-in function in C++ STL that returns an iterator or a constant iterator that 
+    refers to the position where the key is present in the map. 
+    If the key is not present in the map container, it returns an iterator or a constant iterator which 
+    refers to map.end()
+
+    // Syntax: 
+    iterator = map_name.find(key)
+          or 
+    constant iterator = map_name.find(key)
+
+    //HOW TO USE
+    if(m.find(s1)!=m.end()){
+      //if the element is found before the end of the map
+    --> followed by required code!
+
+
+
+*/
