ba7722cbb0173ffae1b0c6ea1f083ff51c6b4e7f
chdaksh01 <96689263+chdaksh01@users.noreply.github.com>
2024-06-04 19:52:36 +0530

    Update bfs.cpp

--- CODES/Graphs/bfs.cpp
+++ CODES/Graphs/bfs.cpp
@@ -1,6 +1,10 @@
 // Writing is IMP, it's complex
 // https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
 
+// Time Complexity: O(N) + O(2E), Where N = Nodes, 2E is for total degrees as we traverse all adjacent nodes.
+
+// Space Complexity: O(3N) ~ O(N), Space for queue data structure visited array and an adjacency list
+
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
